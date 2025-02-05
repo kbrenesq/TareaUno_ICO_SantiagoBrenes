34b80fa00526f286273b3d629c9699df20d3f155
Santiago Brenes <itssanti_@outlook.com>
2025-02-05 17:50:54 -0600

    Add: Initial pages

---
+++ TareaUnoSantiagoBq/src/test/java/com/tareaunosantiagobq/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareaunosantiagobq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
